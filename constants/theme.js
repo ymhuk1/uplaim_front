25142fcf4e5a16b72b24bdfb09b1531efedf7eaf
Alik <makarov.alik1008@gmail.com>
2024-04-08 20:02:55 +0500

    create new file theme.js

---
+++ constants/theme.js
@@ -0,0 +1,17 @@
+const COLORS = {
+  primary: "#181629",
+  iconBack: "#24224A",
+  iconBack2: "#3F3761",
+  text: "#FFFFFF",
+};
+
+const SIZES = {
+  xSmall: 10,
+  small: 12,
+  medium: 16,
+  large: 22,
+  xLarge: 24,
+  xxLarge: 32,
+};
+
+export { COLORS, SIZES };
